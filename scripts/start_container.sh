69a5c217a35cfdc6fe6781735df785d36ff3b347
vardhangude <vrdhnsurya@gmail.com>
2025-10-30 15:41:46 +0530

    should run now: 03:41pm

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker push vardhan018/flask-app:latest
+docker pull vardhan018/flask-app:latest
 # Run the Docker image as a container
 docker run -d -p 5000:5000 vardhan018/flask-app:latest
 echo "Container started and accessible at http://localhost:5000"
