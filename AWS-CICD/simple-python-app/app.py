ee05c2a6814d08ed68bced060c8e1cfbace7ae27
vardhangude <vrdhnsurya@gmail.com>
2025-10-30 17:02:20 +0530

    Python App

--- AWS-CICD/simple-python-app/app.py
+++ AWS-CICD/simple-python-app/app.py
@@ -1,11 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
+# Create the web application
 app = Flask(__name__)
 
+# Define a route for the home page ("/")
 @app.route('/')
-def hello():
-    return 'Hello, Vardhan!'
+def home():
+    # This will look for 'index.html' in the 'templates' folder
+    return render_template('index.html')
 
+# This block runs the app
 if __name__ == '__main__':
-    app.run()
-
+    # host='0.0.0.0' makes it accessible from outside the container
+    app.run(host='0.0.0.0', port=5000)
